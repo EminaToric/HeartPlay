5edb62c6fe65aea3e3d6c65d3fdebede49c88ab9
Emina Toric <137021140+EminaToric@users.noreply.github.com>
2025-06-04 09:48:44 -0400

    Create input.jsx

---
+++ frontend/src/components/ui/input.jsx
@@ -0,0 +1,8 @@
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      className="border p-2 rounded w-full focus:outline-none focus:ring-2 focus:ring-pink-300"
+    />
+  );
+}
