8a264e8808eb056cf4652423a1206ed1d9113337
Emina Toric <137021140+EminaToric@users.noreply.github.com>
2025-06-04 11:27:12 -0400

    Update textarea.jsx

--- frontend/src/components/ui/textarea.jsx
+++ frontend/src/components/ui/textarea.jsx
@@ -2,7 +2,7 @@ export function Textarea({ ...props }) {
   return (
     <textarea
       {...props}
-      className="border p-2 rounded w-full focus:outline-none focus:ring-2 focus:ring-pink-300"
+      className="border border-pink-300 p-2 rounded w-full focus:outline-none focus:ring-2 focus:ring-pink-400 bg-pink-50"
     />
   );
 }
