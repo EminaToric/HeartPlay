ba1fabb2b85c043a22ba1302a4fdb908d887663a
Emina Toric <137021140+EminaToric@users.noreply.github.com>
2025-06-04 11:24:34 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HeartPlayApp from "./HeartPlayApp";
+import "./index.css";
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
